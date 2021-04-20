f8ab6886267a6b59daa35e6188cfef4254b1913f
Curt Barnard <cbarnard@rapid7.com>
2021-04-20 10:44:34 -0700

    Initial commit with URI format

---
+++ CPE2_3_URI.js
@@ -0,0 +1,207 @@
+const fs = require('fs');
+const ohm = require('ohm-js');
+
+const { AttributeError, GrammarError } = require('./Errors');
+
+let CPE2_3_URI = class {
+  static VALID_ATTRS = [
+    'prefix',
+    'part',
+    'vendor',
+    'product',
+    'version',
+    'update',
+    'edition',
+    'language',
+  ];
+
+  constructor(cpeString) {
+    let attrs = CPE2_3_URI.parseCpeString(cpeString);
+
+    // Ensure every attribute is valid for WFN
+    if(!Object.getOwnPropertyNames(attrs).every((attrName) => CPE2_3_URI.VALID_ATTRS.includes(attrName))) {
+      throw new AttributeError('Invalid attribute name provided to constructor.');
+    }
+
+    // TODO: Make sure all attribute values are valid.
+
+    this.attrs = attrs;
+  };
+
+  static parseCpeString(cpeString) {
+    const grammarSpecification = fs.readFileSync('./grammars/cpe2_3uri.ohm');
+    const grammar = ohm.grammar(grammarSpecification.toString());
+    const matcher = grammar.matcher();
+
+    if(!grammar.match(cpeString).succeeded()) {
+      throw new GrammarError("Failed to parse string as a CPE 2.3 URI");
+    }
+
+    matcher.setInput(cpeString);
+    const r = matcher.match();
+    const semantics = grammar.createSemantics();
+
+    semantics.addAttribute('attributes', {
+      cpe_name(prefix, component_list) { return {prefix: prefix.attributes, ...component_list.attributes } },
+      component_list(part) {
+        return {
+          ...part.attributes,
+        }
+      },
+      component_list_two_parts(part, _, vendor) {
+        return {
+          ...part.attributes,
+          ...vendor.attributes,
+        }
+      },
+      component_list_three_parts(part, _1, vendor, _2, product) {
+        return {
+          ...part.attributes,
+          ...vendor.attributes,
+          ...product.attributes,
+        }
+      },
+      component_list_four_parts(part, _1, vendor, _2, product, _3, version) {
+        return {
+          ...part.attributes,
+          ...vendor.attributes,
+          ...product.attributes,
+          ...version.attributes,
+        }
+      },
+      component_list_five_parts(part, _1, vendor, _2, product, _3, version, _4, update) {
+        return {
+          ...part.attributes,
+          ...vendor.attributes,
+          ...product.attributes,
+          ...version.attributes,
+          ...update.attributes,
+        }
+      },
+      component_list_six_parts(part, _1, vendor, _2, product, _3, version, _4, update, _5, edition) {
+        return {
+          ...part.attributes,
+          ...vendor.attributes,
+          ...product.attributes,
+          ...version.attributes,
+          ...update.attributes,
+          ...edition.attributes,
+        }
+      },
+      component_list_seven_parts(part, _1, vendor, _2, product, _3, version, _4, update, _5, edition, _6, language) {
+        return {
+          ...part.attributes,
+          ...vendor.attributes,
+          ...product.attributes,
+          ...version.attributes,
+          ...update.attributes,
+          ...edition.attributes,
+          ...language.attributes,
+        }
+      },
+      part(_) { return {part: this.sourceString} },
+      vendor(_) { return {vendor: this.sourceString} },
+      product(_) { return {product: this.sourceString} },
+      version(_) { return {version: this.sourceString} },
+      update(_) { return {update: this.sourceString} },
+      edition(_) { return {edition: this.sourceString} },
+      language(_) { return {language: this.sourceString} },
+      _terminal() {
+        return this.sourceString ;
+      }
+    });
+
+    return semantics(r).attributes;
+  };
+
+  static generateCpeStringFromAttributes(attrs) {
+    let cpeString = "cpe:/";
+    let stringBuilder = "";
+
+    const {
+      part,
+      vendor   = '*',
+      product  = '*',
+      version  = '*',
+      update   = '*',
+      edition  = '*',
+      language = '*',
+    } = attrs;
+
+    cpeString = `${cpeString}${part}`;
+
+    // Check value of vendor, append to the end if it isn't the final value and a wildcard
+    stringBuilder = `${stringBuilder}:${vendor}`;
+    if (vendor !== '*') {
+      cpeString = `${cpeString}${stringBuilder}`;
+      stringBuilder = '';
+    }
+
+    // Check value of product, append to the end if it isn't the final value and a wildcard
+    stringBuilder = `${stringBuilder}:${product}`;
+    if (product !== '*') {
+      cpeString = `${cpeString}${stringBuilder}`;
+      stringBuilder = '';
+    }
+
+    // Check value of version, append to the end if it isn't the final value and a wildcard
+    stringBuilder = `${stringBuilder}:${version}`;
+    if (version !== '*') {
+      cpeString = `${cpeString}${stringBuilder}`;
+      stringBuilder = '';
+    }
+
+    // Check value of update, append to the end if it isn't the final value and a wildcard
+    stringBuilder = `${stringBuilder}:${update}`;
+    if (update !== '*') {
+      cpeString = `${cpeString}${stringBuilder}`;
+      stringBuilder = '';
+    }
+
+    // Check value of edition, append to the end if it isn't the final value and a wildcard
+    stringBuilder = `${stringBuilder}:${edition}`;
+    if (edition !== '*') {
+      cpeString = `${cpeString}${stringBuilder}`;
+      stringBuilder = '';
+    }
+
+    // Check value of language, append to the end if it isn't the final value and a wildcard
+    stringBuilder = `${stringBuilder}:${language}`;
+    if (language !== '*') {
+      cpeString = `${cpeString}${stringBuilder}`;
+    }
+
+    return cpeString;
+
+  };
+
+  getAttributeValues(attributeName) {
+    // Ensure the provided attribute is valid.
+    if(!CPE2_3_URI.VALID_ATTRS.includes(attributeName)) throw new AttributeError('Invalid attribute.')
+
+    return this.attrs[attributeName] || 'ANY';
+  };
+
+  toString() {
+    let out = "";
+    CPE2_3_URI.VALID_ATTRS.forEach((attrName) => {
+      if(attrName in this.attrs) {
+        if(attrName === 'prefix') {
+          out += `${this.attrs[attrName]}`;
+        } else {
+          out += `${this.attrs[attrName]}:`;
+        }
+
+      }
+    });
+
+    if(out.length > 5) return out.slice(0, -1);
+
+    return out;
+  }
+
+};
+
+
+
+module.exports = CPE2_3_URI;
---
+++ Errors.js
@@ -0,0 +1,8 @@
+
+class AttributeError extends Error {}
+class GrammarError extends Error {}
+
+module.exports = {
+  AttributeError,
+  GrammarError
+};
